8f3a13c094d8230320f55b7af2232276aee2ad32
Daniel Amado <danielfep.am@gmail.com>
2021-03-10 12:20:38 -0500

    Hot fix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,24 +8,19 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *new_node = NULL;
 
 	if (!parent)
 		return (NULL);
-
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
 	if (!new_node)
 		return (NULL);
-
 	if (parent->right)
 	{
 		parent->right->parent = new_node;
-		new_node->left = parent->right;
+		new_node->right = parent->right;
 	}
-	new_node->parent = parent;
 	parent->right = new_node;
-	new_node->left = NULL;
-	new_node->n = value;
 
 	return (new_node);
 }
