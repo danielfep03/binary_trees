33910fc4b3fcf0409ffb502e69a6c98d5ba91bb5
Daniel Amado <danielfep.am@gmail.com>
2021-03-10 00:56:50 -0500

    Validate  if value exists

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 	binary_tree_t *left_node;
 
-	if (!parent)
+	if (!parent || !value)
 		return (NULL);
 
 	new_node = malloc(sizeof(binary_tree_t));
